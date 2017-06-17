b510170aab4605bb89fba0a2a83c681c3d20d877
bugbear <yuhe.wei@upai.com>
2017-06-17 16:29:35 +0800

    change config add load config by env avariable

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,10 @@ RUN apt-get update && apt-get install -my \
   curl \
   git \
   wget \
-  php7.0-cli \
-  php7.0-dev\
+  php7.0-dev \
+  php7.0-mbstring \
   composer \
   php-xdebug
-
 ENV APP_DIR=/usr/local/webapp
 WORKDIR $APP_DIR
 RUN wget https://github.com/swoole/swoole-src/archive/v1.9.13.tar.gz
@@ -17,7 +16,7 @@ WORKDIR  $APP_DIR/swoole-src-1.9.13
 RUN phpize
 RUN ./configure
 RUN make && make install
-RUN echo "extension=swoole.so" >> /etc/php/7.0/cli/conf.d/swoole.MAINTAINER
+RUN echo "extension=swoole.so" >> /etc/php/7.0/cli/conf.d/swoole.ini
 WORKDIR $APP_DIR
 RUN git clone https://github.com/racecourse/courser
 WORKDIR $APP_DIR/courser
--- src/Http/Header.php
+++ src/Http/Header.php
@@ -95,19 +95,11 @@ class Header
     {
         return [
             'SERVER_PROTOCOL' => 'HTTP/1.1',
-            'REQUEST_METHOD' => 'GET',
-            'SCRIPT_NAME' => '',
-            'REQUEST_URI' => '',
-            'QUERY_STRING' => '',
-            'SERVER_PORT' => 80,
-            'HTTP_HOST' => 'localhost',
             'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.8',
             'HTTP_ACCEPT_CHARSET' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-            'X-Powered-By' => 'Courser',
-            'REMOTE_ADDR' => '127.0.0.1',
+            'X-Powered-By' => 'courser',
             'REQUEST_TIME' => time(),
-            'REQUEST_TIME_FLOAT' => microtime(true),
         ];
     }
 }
